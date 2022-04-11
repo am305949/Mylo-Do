41d66167a122bafc18e49941d0a753f46d9ed909
Ahmed.M.Ibrahim <35659429+am305949@users.noreply.github.com>
2022-04-11 14:57:44 +0200

    Add files via upload

---
+++ src/app/models/todos.ts
@@ -0,0 +1,10 @@
+export default class ToDo {
+  _id: string;
+  _groupId: string;
+  _userId: string;
+  title: string;
+  discription: string;
+  Completed: boolean;
+  createdDate: string;
+  finishDate: string;
+}
